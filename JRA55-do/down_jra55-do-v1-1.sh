527d38d3fd558e6389c5c63e767cc795f872b744
Paola Petrelli <paolap@users.noreply.github.com>
2019-09-12 09:14:45 +1000

    Update down_jra55-do-v1-1.sh

--- JRA55-do/down_jra55-do-v1-1.sh
+++ JRA55-do/down_jra55-do-v1-1.sh
@@ -1,5 +1,5 @@
 #cd /g/data1/ua8/JRA55-do/v1-1/
 cd /g/data1/ua8/JRA55-do/suppl/
-#  wgeto--user ppetrelli --password PEV12a74 -c ftp://ftp.ceda.ac.uk/badc/euroclim500/data/GHG/$ens/opm/temp_*.n
+#  wgeto--user  --password  -c ftp://ftp.ceda.ac.uk/badc/euroclim500/data/GHG/$ens/opm/temp_*.n
 #wget -r -c -nH -np -nd -e robots=off http://amaterasu.ees.hokudai.ac.jp/~tsujino/JRA55-do-v1.1/ 
 wget -r -c -nH -np -nd -e robots=off http://amaterasu.ees.hokudai.ac.jp/~tsujino/JRA55-do-suppl/ 
